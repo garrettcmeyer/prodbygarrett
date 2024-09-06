3f91e772e5d669207321e38f2c2cb78196a5c86e
garrettcmeyer <Garrettcmeyer@gmail.com>
2024-09-06 14:00:07 -0700

    update with resume

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import MusicPortfolio from "@/components/MusicPortfolio";
+import LinkedInStyleProfile from "@/components/LinkedInStyleProfile";
 import { useState } from 'react';
 import RecordingSessionBookingPopup from '@/components/RecordingSessionBookingPopup';
 
@@ -12,26 +13,27 @@ export default function Home() {
   const closePopup = () => setIsPopupOpen(false);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 lg:p-24">
-      {/* Existing content */}
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {/* ... existing code ... */}
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[200px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[120px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="flex min-h-screen flex-col items-center justify-between relative text-black bg-gray-100">
+      <div className="relative z-[1] flex place-items-center w-full">
         {/* ... existing code ... */}
       </div>
 
       {/* Add MusicPortfolio component */}
-      <MusicPortfolio />
+      <div className="w-full">
+        <MusicPortfolio />
+      </div>
+      
+      {/* Add space between portfolio and LinkedIn profile */}
+      <div className="w-full h-16"></div>
+      
+      {/* Add LinkedInStyleProfile component */}
+      <div className="w-full">
+        <LinkedInStyleProfile />
+      </div>
 
-      {/* Add button to open popup */}
-      <button
-        onClick={openPopup}
-        className="px-4 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-      >
-        Book Recording Session
-      </button>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex p-4 sm:p-8 lg:p-24">
+        {/* ... existing code ... */}
+      </div>
 
       {/* Render popup component conditionally */}
       {isPopupOpen && (
